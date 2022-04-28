68fdc31c457a1b01a39f9aed676e29ef92779945
Aidan Tomcy <aidantomcy@gmail.com>
2022-04-28 20:06:39 +0530

    Initial Commit

---
+++ index.php
@@ -0,0 +1,211 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link
+      rel="stylesheet"
+      href="css/bootstrap.min.css"
+    />
+    <link
+      rel="stylesheet"
+      href="css/index.css"
+    />
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Poppins&display=swap"
+      rel="stylesheet"
+    />
+    <link
+      rel="apple-touch-icon"
+      sizes="57x57"
+      href="icon/apple-icon-57x57.png"
+    />
+    <link
+      rel="apple-touch-icon"
+      sizes="60x60"
+      href="icon/apple-icon-60x60.png"
+    />
+    <link
+      rel="apple-touch-icon"
+      sizes="72x72"
+      href="icon/apple-icon-72x72.png"
+    />
+    <link
+      rel="apple-touch-icon"
+      sizes="76x76"
+      href="icon/apple-icon-76x76.png"
+    />
+    <link
+      rel="apple-touch-icon"
+      sizes="114x114"
+      href="icon/apple-icon-114x114.png"
+    />
+    <link
+      rel="apple-touch-icon"
+      sizes="120x120"
+      href="icon/apple-icon-120x120.png"
+    />
+    <link
+      rel="apple-touch-icon"
+      sizes="144x144"
+      href="icon/apple-icon-144x144.png"
+    />
+    <link
+      rel="apple-touch-icon"
+      sizes="152x152"
+      href="icon/apple-icon-152x152.png"
+    />
+    <link
+      rel="apple-touch-icon"
+      sizes="180x180"
+      href="icon/apple-icon-180x180.png"
+    />
+    <link
+      rel="icon"
+      type="image/png"
+      sizes="192x192"
+      href="icon/android-icon-192x192.png"
+    />
+    <link
+      rel="icon"
+      type="image/png"
+      sizes="32x32"
+      href="icon/favicon-32x32.png"
+    />
+    <link
+      rel="icon"
+      type="image/png"
+      sizes="96x96"
+      href="icon/favicon-96x96.png"
+    />
+    <link
+      rel="icon"
+      type="image/png"
+      sizes="16x16"
+      href="icon/favicon-16x16.png"
+    />
+    <link
+      rel="manifest"
+      href="icon/manifest.json"
+    />
+    <meta name="msapplication-TileColor" content="#ffffff" />
+    <meta
+      name="msapplication-TileImage"
+      content="icon/ms-icon-144x144.png"
+    />
+    <meta name="theme-color" content="#ffffff" />
+    <meta name="description" content="This is a website about a few saints." />
+    <title>Saints for Us - Home</title>
+  </head>
+  <body>
+    <?php include "inc/navbar.php"; ?>
+
+    <div class="container-fluid">
+      <h1 class="title pt-4">Saints for Us</h1>
+      <div class="quote">
+        <h1>"Your word is a lamp to guide my feet and a light for my path."</h1>
+        <h1 class="align-right">-Psalm 119:105</h1>
+      </div>
+      <div class="images-right">
+        <img
+          src="img/jesus-christ-home-image.jpeg"
+          alt=""
+          class="img-1"
+          loading="lazy"
+        />
+        <img
+          src="img/jesus-christ-home-image2.jpeg"
+          alt=""
+          class="img-2"
+          loading="lazy"
+        />
+      </div>
+      <div class="links">
+        <h2>
+          <a href="stanthony.php">St. Anthony of Padua</a>
+        </h2>
+        <h2>
+          <a href="stfrancisassisi.php">St. Francis of Assisi</a>
+        </h2>
+        <h2>
+          <a href="staidanlindisfarne.php">St. Aidan of Lindisfarne</a>
+        </h2>
+        <h2><a href="stfrancisxavier.php">St. Francis Xavier</a></h2>
+        <h2>
+          <a href="blessedcarloacutis.php">Blessed Carlo Acutis</a>
+        </h2>
+        <h2><a href="stagnes.php">St. Agnes</a></h2>
+        <h2>
+          <a href="stkuriakose.php">St. Kuriakose Elias Chavara</a>
+        </h2>
+        <h2><a href="stjoan.php">St. Joan of Arc</a></h2>
+        <h2><a href="stpeter.php">St. Peter</a></h2>
+        <h2><a href="stbernadette.php">St. Bernadette</a></h2>
+        <h2>
+          <a href="stjohnbaptist.php">St. John the Baptist</a>
+        </h2>
+        <h2><a href="mothermary.php">Mother Mary</a></h2>
+        <h2><a href="stjoseph.php">St. Joseph</a></h2>
+        <h2><a href="motherteresa.php">Mother Teresa</a></h2>
+        <h2><a href="stthomas.php">St. Thomas</a></h2>
+        <h2><a href="stsebastian.php">St. Sebastian</a></h2>
+        <h2><a href="stjohnpaulii.php">Pope St. John Paul II</a></h2>
+        <h2>
+          <a href="stclareassisi.php">St. Clare of Assisi</a>
+        </h2>
+        <h2><a href="stgeorge.php">St. George</a></h2>
+        <h2><a href="stpaul.php">St. Paul</a></h2>
+        <h2><a href="stvianney.php">St. Vianney</a></h2>
+        <h2><a href="stphilipneri.php">St. Philip Neri</a></h2>
+        <h2><a href="stphilip.php">St. Philip</a></h2>
+        <h2><a href="stjohnxxiii.php">Pope St. John XXIII</a></h2>
+        <h2>
+          <a href="sttherese.php">St. Therese of Liseux</a>
+        </h2>
+        <h2><a href="stnicholas.php">St. Nicholas</a></h2>
+        <h2>
+          <a href="stfrancisdesales.php">St. Francis de Sales</a>
+        </h2>
+      </div>
+      <div class="contact-info">
+        <p>
+          <br />
+          Made by-<a href="https://aidantomcy.github.io"
+            >Aidan Tomcy Champannoor</a
+          ><br />
+          If you have any doubts/complaints/feedback, please mail your queries
+          to
+          <a href="mailto:info@saintsforus.com"
+            >info@saintsforus.com</a
+          >/<a href="mailto:aidantomcy@gmail.com">aidantomcy@gmail.com</a>
+          <br />
+          <a href="/feedback">Submit Feedback</a><br />
+          Source-<a href="https://www.wikipedia.org/">Wikipedia</a>
+        </p>
+      </div>
+    </div>
+
+    <script
+      src="js/bootstrap.bundle.min.js"
+      defer
+    ></script>
+    <script>
+      const links = document.querySelectorAll("h2");
+      const title = document.querySelector("h1");
+      links.forEach((link) => {
+        link.classList.add("pb-2");
+      });
+
+      if (window.innerWidth <= 922) {
+        title.classList.add("pb-4");
+      }
+
+      document.addEventListener("contextmenu", (e) => {
+        e.preventDefault();
+      });
+    </script>
+  </body>
+</html>
---
+++ vendor/composer/installed.php
@@ -0,0 +1,41 @@
+<?php return [
+    "root" => [
+        "pretty_version" => "1.0.0+no-version-set",
+        "version" => "1.0.0.0",
+        "type" => "library",
+        "install_path" => __DIR__ . "/../../",
+        "aliases" => [],
+        "reference" => null,
+        "name" => "__root__",
+        "dev" => true,
+    ],
+    "versions" => [
+        "__root__" => [
+            "pretty_version" => "1.0.0+no-version-set",
+            "version" => "1.0.0.0",
+            "type" => "library",
+            "install_path" => __DIR__ . "/../../",
+            "aliases" => [],
+            "reference" => null,
+            "dev_requirement" => false,
+        ],
+        "phpmailer/phpmailer" => [
+            "pretty_version" => "v6.6.0",
+            "version" => "6.6.0.0",
+            "type" => "library",
+            "install_path" => __DIR__ . "/../phpmailer/phpmailer",
+            "aliases" => [],
+            "reference" => "e43bac82edc26ca04b36143a48bde1c051cfd5b1",
+            "dev_requirement" => false,
+        ],
+        "symfony/dotenv" => [
+            "pretty_version" => "v6.0.5",
+            "version" => "6.0.5.0",
+            "type" => "library",
+            "install_path" => __DIR__ . "/../symfony/dotenv",
+            "aliases" => [],
+            "reference" => "1c2288fdfd0787288cd04b9868f879f2212159c4",
+            "dev_requirement" => false,
+        ],
+    ],
+];
