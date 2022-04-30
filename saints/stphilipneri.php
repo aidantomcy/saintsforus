943c4dd5f113f77a4ae2fb1871608315b9053b63
Aidan Tomcy <aidantomcy@gmail.com>
2022-04-30 08:10:27 +0530

    Add twenty second saint

---
+++ saints/stphilipneri.php
@@ -0,0 +1,240 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Dancing+Script&family=Poppins&display=swap"
+      rel="stylesheet"
+    />
+    <link
+      rel="stylesheet"
+      href="../css/saint.css"
+    />
+    <link
+      rel="apple-touch-icon"
+      sizes="57x57"
+      href="../icon/apple-icon-57x57.png"
+    />
+    <link
+      rel="apple-touch-icon"
+      sizes="60x60"
+      href="../icon/apple-icon-60x60.png"
+    />
+    <link
+      rel="apple-touch-icon"
+      sizes="72x72"
+      href="../icon/apple-icon-72x72.png"
+    />
+    <link
+      rel="apple-touch-icon"
+      sizes="76x76"
+      href="../icon/apple-icon-76x76.png"
+    />
+    <link
+      rel="apple-touch-icon"
+      sizes="114x114"
+      href="../icon/apple-icon-114x114.png"
+    />
+    <link
+      rel="apple-touch-icon"
+      sizes="120x120"
+      href="../icon/apple-icon-120x120.png"
+    />
+    <link
+      rel="apple-touch-icon"
+      sizes="144x144"
+      href="../icon/apple-icon-144x144.png"
+    />
+    <link
+      rel="apple-touch-icon"
+      sizes="152x152"
+      href="../icon/apple-icon-152x152.png"
+    />
+    <link
+      rel="apple-touch-icon"
+      sizes="180x180"
+      href="../icon/apple-icon-180x180.png"
+    />
+    <link
+      rel="icon"
+      type="image/png"
+      sizes="192x192"
+      href="../icon/android-icon-192x192.png"
+    />
+    <link
+      rel="icon"
+      type="image/png"
+      sizes="32x32"
+      href="../icon/favicon-32x32.png"
+    />
+    <link
+      rel="icon"
+      type="image/png"
+      sizes="96x96"
+      href="../icon/favicon-96x96.png"
+    />
+    <link
+      rel="icon"
+      type="image/png"
+      sizes="16x16"
+      href="../icon/favicon-16x16.png"
+    />
+    <link
+      rel="manifest"
+      href="../icon/manifest.json"
+    />
+    <meta name="msapplication-TileColor" content="#ffffff" />
+    <meta
+      name="msapplication-TileImage"
+      content="../icon/ms-icon-144x144.png"
+    />
+    <meta name="theme-color" content="#ffffff" />
+    
+  <style>
+    .container {
+      display: block;
+      margin-left: auto;
+      margin-right: auto;
+      width: 1200px;
+      height: 2800px;
+      background: #ccc;
+    }
+  </style>
+
+    <title>Saints for Us - St. Philip Neri</title>
+  </head>
+  <body>
+    
+  <div class="container">
+    <div class="header">
+      <div class="text">
+        <h1>St. Philip Neri</h1>
+        <p>(22 July 1515-26 May 1595)</p>
+        <p>"A joyful heart is more easily made perfect than a downcast one."</p>
+        <p class="quote-sayer">-St. Philip Neri</p>
+      </div>
+    </div>
+    <div class="content">
+      <div class="image">
+        <img
+          class="img"
+          src="../img/stphilipneri.jpg"
+          alt=""
+        />
+      </div>
+      <div class="text">
+        <p>
+          St. Philip Neri (22 July 1515-26 May 1595), known as the Second Apostle
+          of Rome, after St. Peter, was
+          <br />an Italian priest noted for founding a society of secular clergy
+          called the Congregation of the Oratory. <br />Philip was the son of
+          Francesco di Neri, a lawyer, and his wife Lucrezia da Mosciano, whose
+          family were <br />nobility in the service of the state. He was brought
+          up with great care and received his early teaching <br />from the friars
+          at San Marco, the famous Dominican monastery in Florence. He was
+          accustomed in later life <br />to ascribe most of his progress to the
+          teaching of two of them - Zenobio de'Medici and Servanzio Mini. At
+          <br />the age of 18, Philip was sent to his uncle, Romolo, a wealthy
+          merchant at San Germano, a then Neapolitan <br />town near the base of
+          Monte Cassino, to assist him in his business and with the hope that he
+          might inherit <br />his uncle's fortune. He gained Romolo's confidence
+          and affection, but soon after coming to San Germano, <br />Philip had a
+          religious conversion.
+        </p>
+        <h2>Mission work</h2>
+        <p>
+          After arriving in Rome, Philip became a tutor in the house of a
+          Florentine aristocrat named Galeotto Caccia.
+          <br />After two years he began to pursue his own studies (for a period
+          of three years) under the guidance of the <br />Augustinians. Following
+          this, he began those labours amongst the sick and poor which, in later
+          life, gained <br />him the title of "Apostle of Rome". He also
+          ministered to the prostitutes of the city. In 1538 he entered into
+          <br />the home mission work for which he became famous, traveling
+          throughout the city, seeking opportunities of <br />entering into
+          conversation with people, and of leading them to consider the topics he
+          set before them. For <br />seventeen years Philip lived as a layman in
+          Rome, probably without thinking of becoming a priest. Around 1544,
+          <br />he made the acquaintance of Ignatius of Loyola. Many of Philip's
+          disciples found their vocations in the infant <br />Society of Jesus.
+        </p>
+        <h2>Confraternity of the Holy Trinity</h2>
+        <p>
+          In 1548, together with his confessor, Persiano Rossa, Philip founded the
+          Confraternity of the Most Holy
+          <br />Trinity of Pilgrims and Convalescents (Italian:
+          <em>Santissima Trinita de' Pellegrini e de' Convalescenti</em>),
+          <br />whose primary object was to minister to the needs of the thousands
+          of poor pilgrims who flocked to Rome, <br />especially in jubilee years,
+          and also to relieve the patients discharged from hospitals but who were
+          still <br />too weak for labour. Members met for prayer at the Church of
+          San Salvatore in Campo where the devotion of <br />the Forty Hours of
+          Exposition of the Blessed Sacrament was first introduced into Rome.
+        </p>
+        <h2>Personal character</h2>
+        <p>
+          Philip possessed a playful sense of humour, combined with a shrewd wit.
+          He considered a cheerful temper
+          <br />to be more Christian than a melancholy one, and carried this
+          spirit into his whole life: "A joyful heart <br />is more easily made
+          perfect than a downcast one." This was the secret of Neri's popularity
+          and of his <br />place in the folklore of the Roman poor. Many miracles
+          were attributed to him. When his body was examined <br />after death, it
+          was found that two of his ribs had been broken, which was attributed at
+          the time to the <br />expansion of his heart while fervently praying in
+          the catacombs about the year 1545. Benedict XIV, who <br />reorganised
+          the rules for canonization, decided that Philip's enlarged heart was
+          caused by an aneurism. <br />Ponnelle and Bordet, in their 1932
+          biography
+          <em>St. Philip Neri and the Roman Society of His Times</em> (1515-1595),
+          <br />conclude that it was partly natural and partly supernatural. What
+          is certain is that Philip himself and his <br />penitents associated it
+          with divine love.
+        </p>
+        <h2>Death and veneration</h2>
+        <p>
+          Philip Neri died around the end of the day on 25 May 1595, the Feast of
+          Corpus Christi that year, after
+          <br />having spent the day hearing confessions and receiving visitors.
+          About midnight he began hemorrhaging, <br />and Baronius read the
+          commendatory prayers over him. Baronius asked that he bless his
+          spiritual sons before <br />dying and, though he could no longer speak,
+          he blessed them with the sign of the cross and died. Philip Neri
+          <br />was beatified by Paul V in 1615 and canonized by Pope Gregory XV
+          in 1622. His memorial is celebrated on 26 May. <br />His body is
+          venerated in the Chiesa Nuova ("New Church") in Rome. Philip Neri is one
+          of the influential figures <br />of the Counter-Reformation, and noted
+          for converting to personal holiness many of the influential people
+          within <br />the Church itself.
+        </p>
+      </div>
+    </div>
+  </div>
+  <div class="bottom-links">
+    <div class="prev-next">
+      <a class="btn" href="stvianney.php">
+        <h2>Prev.</h2>
+      </a>
+      &nbsp;&nbsp;&nbsp;
+      <a class="btn" href="stphilip.php">
+        <h2>Next</h2>
+      </a>
+    </div>
+    <a class="btn" href="../index.php">
+      <h2>Back to Home Page</h2>
+    </a>
+  </div>
+
+
+    <script>
+      document.addEventListener("contextmenu", (e) => {
+        e.preventDefault();
+      });
+    </script>
+  </body>
+</html>
