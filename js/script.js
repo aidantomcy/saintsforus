1e9c016c7a8d236e147e53f5fe38bddc23102cb1
Aidan Tomcy <aidantomcy@gmail.com>
2022-05-24 20:50:55 +0530

    Update path to service worker

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ const imgs = document.querySelectorAll("img");
 imgs.forEach((img) => (img.ondragstart = () => false));
 
 if ("serviceWorker" in navigator) {
-  navigator.serviceWorker.register("./js/service-worker.js");
+  navigator.serviceWorker.register("../js/service-worker.js");
 }
 
 document.addEventListener("contextmenu", (e) => {
