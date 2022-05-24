536370f8a54f36a61ed7d50c2b426fb72ff99808
Aidan Tomcy <aidantomcy@gmail.com>
2022-05-24 08:35:50 +0530

    Format file

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 const imgs = document.querySelectorAll("img");
-imgs.forEach((img) => img.ondragstart = () => false)
+imgs.forEach((img) => (img.ondragstart = () => false));
 
 document.addEventListener("contextmenu", (e) => {
   e.preventDefault();
