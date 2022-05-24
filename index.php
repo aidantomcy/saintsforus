15cb1880cb1b23227c484ddc674e86aa333d3a26
Aidan Tomcy <aidantomcy@gmail.com>
2022-05-24 20:41:02 +0530

    Remove links to old icons and import service worker

--- index.php
+++ index.php
@@ -18,85 +18,9 @@
       href="https://fonts.googleapis.com/css2?family=Poppins&display=swap"
       rel="stylesheet"
     />
-    <link
-      rel="apple-touch-icon"
-      sizes="57x57"
-      href="icon/apple-icon-57x57.png"
-    />
-    <link
-      rel="apple-touch-icon"
-      sizes="60x60"
-      href="icon/apple-icon-60x60.png"
-    />
-    <link
-      rel="apple-touch-icon"
-      sizes="72x72"
-      href="icon/apple-icon-72x72.png"
-    />
-    <link
-      rel="apple-touch-icon"
-      sizes="76x76"
-      href="icon/apple-icon-76x76.png"
-    />
-    <link
-      rel="apple-touch-icon"
-      sizes="114x114"
-      href="icon/apple-icon-114x114.png"
-    />
-    <link
-      rel="apple-touch-icon"
-      sizes="120x120"
-      href="icon/apple-icon-120x120.png"
-    />
-    <link
-      rel="apple-touch-icon"
-      sizes="144x144"
-      href="icon/apple-icon-144x144.png"
-    />
-    <link
-      rel="apple-touch-icon"
-      sizes="152x152"
-      href="icon/apple-icon-152x152.png"
-    />
-    <link
-      rel="apple-touch-icon"
-      sizes="180x180"
-      href="icon/apple-icon-180x180.png"
-    />
-    <link
-      rel="icon"
-      type="image/png"
-      sizes="192x192"
-      href="icon/android-icon-192x192.png"
-    />
-    <link
-      rel="icon"
-      type="image/png"
-      sizes="32x32"
-      href="icon/favicon-32x32.png"
-    />
-    <link
-      rel="icon"
-      type="image/png"
-      sizes="96x96"
-      href="icon/favicon-96x96.png"
-    />
-    <link
-      rel="icon"
-      type="image/png"
-      sizes="16x16"
-      href="icon/favicon-16x16.png"
-    />
-    <link
-      rel="manifest"
-      href="icon/manifest.json"
-    />
-    <meta name="msapplication-TileColor" content="#ffffff" />
-    <meta
-      name="msapplication-TileImage"
-      content="icon/ms-icon-144x144.png"
-    />
-    <meta name="theme-color" content="#ffffff" />
+    <meta name="theme-color" content="#b0e0e6" />
+    <link rel="manifest" href="manifest.json">
+    <link rel="shortcut icon" href="icon/favicon.ico" type="image/x-icon">
     <meta 
       name="description" 
       content="This is a website about a few Catholic saints, whose intercession we rely on in our day-to-day lives." 
@@ -209,6 +133,10 @@
 
       if (window.innerWidth <= 922) {
         title.classList.add("pb-4");
+      }
+
+      if ("serviceWorker" in navigator) {
+        navigator.serviceWorker.register("./js/service-worker.js");
       }
 
       document.addEventListener("contextmenu", (e) => {
--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
 const imgs = document.querySelectorAll("img");
 imgs.forEach((img) => (img.ondragstart = () => false));
 
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("./js/service-worker.js");
+}
+
 document.addEventListener("contextmenu", (e) => {
   e.preventDefault();
 });
