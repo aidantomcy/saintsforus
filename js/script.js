efb15a8072164f31a5cb77048a3c768539847c0a
Aidan Tomcy <aidantomcy@gmail.com>
2022-05-28 09:00:17 +0530

    Remove service worker code

--- js/script.js
+++ js/script.js
@@ -1,10 +1,6 @@
 const imgs = document.querySelectorAll("img");
 imgs.forEach((img) => (img.ondragstart = () => false));
 
-if ("serviceWorker" in navigator) {
-  navigator.serviceWorker.register("/service-worker.js");
-}
-
 document.addEventListener("contextmenu", (e) => {
   e.preventDefault();
 });
