f393c0665557983d41138414fc01c96521d70b5b
Aidan Tomcy <aidantomcy@gmail.com>
2023-03-30 10:30:09 +0530

    add navbar

---
+++ src/components/NavItem.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  export let text: string;
+  export let href: string;
+</script>
+
+<a
+  {href}
+  class="text-xl font-normal md:inline-block p-8 rounded-lg text-gray-700 hover:text-black dark:hover:text-white dark:text-grey transition-all inline"
+  >{text}</a
+>
---
+++ src/components/Navbar.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import NavItem from "./NavItem.svelte";
+  import ThemeButton from "./ThemeButton.svelte";
+</script>
+
+<div class="w-full flex justify-center mt-4">
+  <nav
+    class="bg-gray-200 w-1/2 h-14 rounded-xl flex justify-center items-center dark:bg-dark-secondary"
+  >
+    <NavItem text="Home" href="/" />
+    <NavItem text="Feedback" href="/" />
+    <NavItem text="About the Creator" href="https://aidantomcy.github.io" />
+    <ThemeButton />
+  </nav>
+</div>
