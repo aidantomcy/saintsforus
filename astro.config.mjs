b4321149c0ffe8ca3391b4b210df2b85bee07b3f
Aidan Tomcy <aidantomcy@gmail.com>
2023-03-31 08:36:23 +0530

    install partytown integration for astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,9 @@ import svelte from "@astrojs/svelte";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 
+import partytown from "@astrojs/partytown";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte(), tailwind(), mdx()],
+  integrations: [svelte(), tailwind(), mdx(), partytown()]
 });
