84e83db5e9dfea5d84cdada99d600816c9432c03
Aidan Tomcy <aidantomcy@gmail.com>
2023-04-02 11:08:55 +0530

    remove body and alias field from saints collection and added lifetime field

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,8 +3,7 @@ import { defineCollection, z } from "astro:content";
 const saints = defineCollection({
   schema: z.object({
     title: z.string(),
-    alias: z.string(),
-    body: z.string(),
+    lifetime: z.string(),
   }),
 });
 
