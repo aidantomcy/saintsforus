f5ab3fb2dc8e1a6aecf9cce3d931632c13baa35e
Aidan Tomcy <aidantomcy@gmail.com>
2023-04-02 11:06:55 +0530

    add button to go back to home page

---
+++ src/components/BackButton.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import Back from "./_icons/Back.svelte";
+</script>
+
+<a
+  href="/"
+  class="text-xl flex items-center justify-center w-24 bg-slate-100 mb-4 mt-12 p-2 rounded-md hover:bg-slate-200 hover:shadow-sm dark:bg-dark-secondary dark:text-white h-12 hover:-translate-y-[0.125rem] hover:transition duration-75 ml-80"
+>
+  <Back />
+  Back
+</a>
