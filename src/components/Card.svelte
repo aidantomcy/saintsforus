413aa8c00174d5907f05bba00f18bc0f187350f4
Aidan Tomcy <aidantomcy@gmail.com>
2023-04-02 11:07:18 +0530

    updated card component for home page

---
+++ src/components/Card.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  export let title: string;
+  export let image: string;
+  export let href: string;
+</script>
+
+<a
+  class="my-6 bg-gray-200 p-4 rounded-xl grid place-items-center select-none h-80 w-52 mx-40 hover:-translate-y-1 hover:cursor-pointer hover:shadow-lg hover:duration-150 dark:bg-dark-secondary dark:text-white"
+  {href}
+>
+  <img src={image} alt="" class="rounded-2xl w-40 h-52 object-cover" />
+  <div class="text-center py-2">{title}</div>
+</a>
