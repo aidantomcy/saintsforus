29d1be26e0db18da40517f9712cc890e1882a52b
Aidan Tomcy <aidantomcy@gmail.com>
2023-04-02 11:21:15 +0530

    enable lazy loading of images in card component

--- src/components/Card.svelte
+++ src/components/Card.svelte
@@ -8,6 +8,11 @@
   class="my-6 bg-gray-200 p-4 rounded-xl grid place-items-center select-none h-80 w-52 mx-40 hover:-translate-y-1 hover:cursor-pointer hover:shadow-lg hover:duration-150 dark:bg-dark-secondary dark:text-white"
   {href}
 >
-  <img src={image} alt="" class="rounded-2xl w-40 h-52 object-cover" />
+  <img
+    src={image}
+    alt=""
+    class="rounded-2xl w-40 h-52 object-cover"
+    loading="lazy"
+  />
   <div class="text-center py-2">{title}</div>
 </a>
